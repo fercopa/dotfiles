d0994862a4e4439ad2c8482c1e68cf3e0d6ed0a5
Fernando Copa <fernando.copa@tunubi.com>
2024-04-08 22:37:17 -0300

    Fixed cmp completation

--- nvim/.config/nvim/lua/plugins/cmp.lua
+++ nvim/.config/nvim/lua/plugins/cmp.lua
@@ -13,11 +13,6 @@ return {
             border = "rounded",
             winhighlight = "Normal:NormalFloat,FloatBorder:FloatBorder,CursorLine:PmenuSel,Search:None",
         }
-        local function has_words_before()
-            -- local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-            local line, col = table.unpack(vim.api.nvim_win_get_cursor(0))
-            return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match "%s" == nil
-        end
         -- Set configuration for specific filetype.
         cmp.setup.filetype('gitcommit', {
             sources = cmp.config.sources({
@@ -66,10 +61,6 @@ return {
                 ["<Tab>"] = cmp.mapping(function(fallback)
                     if cmp.visible() then
                         cmp.select_next_item()
-                    -- elseif luasnip.expand_or_jumpable() then
-                    --     luasnip.expand_or_jump()
-                    elseif has_words_before() then
-                        cmp.complete()
                     else
                         fallback()
                     end
@@ -77,8 +68,6 @@ return {
                 ["<S-Tab>"] = cmp.mapping(function(fallback)
                     if cmp.visible() then
                         cmp.select_prev_item()
-                    -- elseif luasnip.jumpable(-1) then
-                    --     luasnip.jump(-1)
                     else
                         fallback()
                     end
